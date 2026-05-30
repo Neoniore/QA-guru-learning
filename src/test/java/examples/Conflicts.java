15557234ce8fc1d72651161ef034ab08bd04570c
Neoniore <31822720+Neoniore@users.noreply.github.com>
2026-05-30 14:15:52 +0300

    Update greeting in conflictCreator method

--- src/test/java/examples/Conflicts.java
+++ src/test/java/examples/Conflicts.java
@@ -2,7 +2,7 @@ package examples;
 
 public class Conflicts {
     public static void conflictCreator() {
-        System.out.println("hello!");
+        System.out.println("hello, WORLD!!!!!");
     }
 
 }
