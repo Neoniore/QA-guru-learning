03bec44e6c8ae760fc778c360487b31e318703b3
Aleksandr Panchenko <lforjust@gmail.com>
2026-05-30 14:12:15 +0300

    greeting changed

--- src/test/java/examples/Conflicts.java
+++ src/test/java/examples/Conflicts.java
@@ -2,7 +2,8 @@ package examples;
 
 public class Conflicts {
     public static void conflictCreator() {
-        System.out.println("hello!");
+        System.out.println("hello,");
+        System.out.println("World!");
     }
 
 }
