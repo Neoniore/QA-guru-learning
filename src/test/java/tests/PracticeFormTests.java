6a8205cc1176865e620a124bf7780b01247cbdad
Aleksandr Panchenko <lforjust@gmail.com>
2026-06-17 21:49:36 +0300

    added parametrized tests

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,39 +1,41 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-
+@DisplayName("Тесты регистрации")
 public class PracticeFormTests extends BaseTest {
 
-
-    @Test
-    public void positiveRegistrationFormFillingTest() {
-        $("[id='firstName']").setValue("Aleks");
-        $("[id='lastName']").setValue("Pechkin");
+    @DisplayName("Регистрация с валидным заполнением всех полей")
+    @ParameterizedTest()
+    @CsvFileSource(resources = "/test_data/positiveRegistrationFormFillingTest.csv")
+    public void positiveRegistrationFormFillingTest(String name, String surname, String phone) {
+        $("[id='firstName']").setValue(name);
+        $("[id='lastName']").setValue(surname);
         $("[id='userEmail']").setValue("Aleks@sdfs.com");
         $("[id='genterWrapper']").$(byText("Male")).click();
-        $("[id='userNumber']").setValue("8999999997");
-
+        $("[id='userNumber']").setValue(phone);
         $("[id='dateOfBirthInput']").click();
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("1998");
         $(".react-datepicker__day--001").click();
-
         $("[id='subjectsInput']").setValue("E")
                 .pressEnter()
                 .setValue("computer science")
                 .pressEnter();
-
         $("[id='hobbiesWrapper']").$(byText("Sports")).click();
         $("[id='hobbiesWrapper']").$(byText("Reading")).click();
         $("[id='hobbiesWrapper']").$(byText("Music")).click();
 
-                $("[id='uploadPicture']").uploadFromClasspath("image.jpg");
+        $("[id='uploadPicture']").uploadFromClasspath("image.jpg");
 
         $("[id='currentAddress']").setValue("Backer street, 221b");
 
@@ -45,32 +47,23 @@ public class PracticeFormTests extends BaseTest {
 
 
         $x("//tr[td[text()='Student Name']]/td[2]")
-                .shouldBe(text("Aleks Pechkin"));
-
+                .shouldBe(text(name + " " + surname));
         $x("//tr[td[text()='Student Email']]/td[2]")
                 .shouldBe(text("Aleks@sdfs.com"));
-
         $x("//tr[td[text()='Gender']]/td[2]")
                 .shouldBe(text("Male"));
-
         $x("//tr[td[text()='Mobile']]/td[2]")
-                .shouldBe(text("8999999997"));
-
+                .shouldBe(text(phone));
         $x("//tr[td[text()='Date of Birth']]/td[2]")
                 .shouldBe(text("01 July,1998"));
-
         $x("//tr[td[text()='Subjects']]/td[2]")
                 .shouldBe(text("English, Computer Science"));
-
         $x("//tr[td[text()='Hobbies']]/td[2]")
                 .shouldBe(text("Sports, Reading, Music"));
-
         $x("//tr[td[text()='Picture']]/td[2]")
                 .shouldBe(text("image.jpg"));
-
         $x("//tr[td[text()='Address']]/td[2]")
                 .shouldBe(text("Backer street, 221b"));
-
         $x("//tr[td[text()='State and City']]/td[2]")
                 .shouldBe(text("Uttar Pradesh Agra"));
     }
@@ -152,3 +145,6 @@ public class PracticeFormTests extends BaseTest {
         $(".table-responsive").shouldNotBe(visible);
     }
 }
+
+
+
