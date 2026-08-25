000b49c7918478c692042ae822c413e488592fd8
Aleksandr Panchenko <lforjust@gmail.com>
2026-08-25 18:05:26 +0300

    add attachments

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -5,8 +5,6 @@ import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
